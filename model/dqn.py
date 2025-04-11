e602e05c1e14e20a6ebb39b0a8ea936f9d2fd70c
Naji Danial <ndanial.ieu2022@student.ie.edu>
2025-04-11 20:55:27 +0200

    Update dqn.py

--- model/dqn.py
+++ model/dqn.py
@@ -1,1 +1,17 @@
+import torch
+import torch.nn as nn
+
+class DQN(nn.Module):
+    def __init__(self):
+        super(DQN, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(9, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 9)
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
