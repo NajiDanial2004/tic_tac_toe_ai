65cceb8e4750481bd950cdd1537c21ea82a9fa81
Naji Danial <ndanial.ieu2022@student.ie.edu>
2025-04-11 20:55:42 +0200

    Update replay_buffer.py

--- model/replay_buffer.py
+++ model/replay_buffer.py
@@ -1,1 +1,26 @@
+import random
+import numpy as np
+import torch
+from collections import deque
+
+class ReplayBuffer:
+    def __init__(self, capacity=10000):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = map(np.array, zip(*batch))
+        return (
+            torch.FloatTensor(state),
+            torch.LongTensor(action),
+            torch.FloatTensor(reward),
+            torch.FloatTensor(next_state),
+            torch.FloatTensor(done)
+        )
+
+    def __len__(self):
+        return len(self.buffer)
 
